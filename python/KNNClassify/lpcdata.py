70a73b8c82e5707e0d78b15a0d433a81a05d178e
Dhanesh Pradhan <dpradhan@eng.ucsd.edu>
2016-05-30 17:42:50 -0700

    Calculate LPC

---
+++ python/KNNClassify/lpcdata.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import numpy as np
+import scipy as sp
+import scipy.signal as sig
+
+def lpc_ref(signal, order):
+    if signal.ndim > 1:
+        raise ValueError("Array of rank > 1 not supported yet")
+    if order > signal.size:
+        raise ValueError("Input signal must have a lenght >= lpc order")
+
+    if order > 0:
+        p = order + 1
+        r = np.zeros(p, signal.dtype)
+        # Number of non zero values in autocorrelation one needs for p LPC
+        # coefficients
+        nx = np.min([p, signal.size])
+        x = np.correlate(signal, signal, 'full')
+        r[:nx] = x[signal.size-1:signal.size+order]
+        phi = np.dot(sp.linalg.inv(sp.linalg.toeplitz(r[:-1])), -r[1:])
+        return np.concatenate(([1.], phi))
+    else:
+        return np.ones(1, dtype = signal.dtype)
