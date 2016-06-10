6e7e2695c45ae785996161c40769b6ac6278bae9
nmmsv <nima.moosavi@gmail.com>
2016-06-09 17:22:49 -0700

    commented security

--- python/ContextEngineBase.py
+++ python/ContextEngineBase.py
@@ -4,13 +4,13 @@ import math
 import numpy as np
 import sys
 
-sys.path.append("../python/Security/Encrypt/")
-from encrypt import encrypt
-from encrypt import rsaEncrypt
-sys.path.remove("../python/Security/Encrypt/")
-sys.path.append("../python/Security/Decrypt/")
-from decrypt import rsaDecrypt
-from decrypt import decrypt
+#sys.path.append("../python/Security/Encrypt/")
+#from encrypt import encrypt
+#from encrypt import rsaEncrypt
+#sys.path.remove("../python/Security/Encrypt/")
+#sys.path.append("../python/Security/Decrypt/")
+#from decrypt import rsaDecrypt
+#from decrypt import decrypt
 
 class Complexity(Enum):
     firstOrder  = 1
