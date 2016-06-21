b80daecf048bdc3df555e575c4f73e32cbf0b301
Cherish MuMu <cherish.cui.122@gmail.com>
2016-06-21 15:33:37 -0700

    inherit CE base

--- python/Svr/svr.py
+++ python/Svr/svr.py
@@ -3,62 +3,16 @@
 import numpy as np
 import math
 from sklearn.svm import SVR
-from ContextEngineBase import *
+from ContextEngineBase import ContextEngineBase
 import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 ## Implementation of the SVR algorithm:
-class SVR:
-    complexity = 0;
+class SVR(ContextEngineBase):
 
-    numInputs = 0;
-
-    discreteOutput = 0;
-
-    discreteInputs = [];
-
-    numObservations = 0;
-
-    x_Obs = np.empty([0, 0]);
-
-    y_Obs = np.empty([0]);
-
-    y_Test = np.empty([0]);
-
-	#trained result
-	#knnRegressor = KNeighborsRegressor(n_neighbors=2, weights='uniform');
-    svrLinear = SVR(kernel='rbf');
-
-    def __init__(self, complexity, numInputs, discreteOutputs, discreteInputs):
-		self.complexity = complexity;
-		self.numInputs = numInputs;
-		self.discreteOutputs = discreteOutputs;
-		self.discreteInputs = discreteInputs;
-		self.x_Obs = np.empty([0,numInputs]);
-		self.x_Test = np.empty([0,numInputs]);
-
-    def addSingleObservation(self, newInputObs, newOutputObs):
-		if (len(newInputObs) == self.numInputs and type(newOutputObs) not in (tuple, list)):
-			#print("All good!");
-			self.x_Obs = np.vstack((self.x_Obs,newInputObs));
-			self.y_Obs = np.append(self.y_Obs, newOutputObs);
-			self.numObservations += 1;
-		else:
-			print("Wrong dimensions!");
-
-    def addBatchObservations(self, newInputObsMatrix, newOutputVector):
-
-		if(len(newInputObsMatrix.shape) == 2 and newInputObsMatrix.shape[1] == self.numInputs
-			and newOutputVector.shape[0] == newInputObsMatrix.shape[0]):
-			#print("All good!");
-			newOutputVector = newOutputVector.ravel();
-			i = 0;
-			for newInputVector in newInputObsMatrix:
-				newOutputValue = newOutputVector[i];
-				self.addSingleObservation(newInputVector, newOutputValue);
-				i += 1;
-		else:
-			print("Wrong dimensions!");
+    def __init__(self, complexity, numInputs, outputClassifier, inputClassifiers, appFieldsDict):
+		ContextEngineBase.__init__(self,complexity, numInputs, outputClassifier, inputClassifiers, appFieldsDict)
+		self.svrLinear = SVR(kernel='rbf');
 
     def train(self):
 		if (self.numObservations > 0):
